443939ab215db7eb1995c3a7ccefa25cc8de1d0d
tranvannhan1911 <tranvannhan1911@gmail.com>
2022-08-24 19:21:50 +0700

    fix staticmethod

--- management/utils/swagger.py
+++ management/utils/swagger.py
@@ -2,6 +2,7 @@ from drf_yasg import openapi
 from management.serializers import ResponeSuccessSerializer
 from management.utils.apicode import ApiCode
 class SwaggerSchema():
+    @staticmethod
     def success():
         return openapi.Response(
             description="Successful",
